94114fcdc0a4e0fee79103976902494167adae66
GreenCrowDev <pcalifano@outlook.it>
2024-03-21 22:38:38 +0100

    Add `Flip` bool to HNCurveToMesh

--- src/hoodie_nodes/curve_operations/hn_curve_to_mesh.h
+++ src/hoodie_nodes/curve_operations/hn_curve_to_mesh.h
@@ -14,8 +14,16 @@ private:
     Array curve;
     Array profile;
     bool shape_is_closed;
+    bool flip;
     // Output
     Array mesh;
+
+public:
+    void set_flip(const bool p_value);
+    bool get_flip() const;
+
+protected:
+    static void _bind_methods();
 
 public:
     void _process(const Array &p_inputs) override;
@@ -29,6 +37,14 @@ public:
     int get_output_port_count() const override;
 	PortType get_output_port_type(int p_port) const override;
 	String get_output_port_name(int p_port) const override;
+
+    int get_property_input_count() const override;
+    Variant::Type get_property_input_type(vec_size_t p_prop) const override;
+    Variant get_property_input(vec_size_t p_port) const override;
+    void set_property_input(vec_size_t p_prop, Variant p_input) override;
+
+    Vector<StringName> get_editable_properties() const override;
+    HashMap<StringName, String> get_editable_properties_names() const override;
 
     const Variant get_output(int p_port) const override;
 };
